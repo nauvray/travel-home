fea921f53eec231e6dfca5bb28ae6ce0cab2dce3
maddalen lepphaille <maddalen.lepphaille@gmail.com>
2023-03-09 10:39:11 +0100

    Pre-processing fixed

--- travel-home-app/main.py
+++ travel-home-app/main.py
@@ -1,10 +1,12 @@
 import time
 import preprocess as preproc
+import utils
+import pandas as pd
 
-def preprocess(root_folder : str, csv_name : str):
+def preprocess(root_folder : str, csv_name : str, df : pd.DataFrame):
     # filter outdoor images
     start_time = time.time()
-    df = preproc.filter_outdoor_images(root_folder, csv_name)
+    df = preproc.filter_outdoor_images(df)
     print('==> Time to filter outdoor images:', round(time.time() - start_time, 1), 'seconds')
 
     # save images as npy files
@@ -20,8 +22,8 @@ def preprocess(root_folder : str, csv_name : str):
 if __name__ == '__main__':
     root_folder = '../00-data/sample/'
     csv_name = 'meta_shard_0.csv'
+    # df : img lat lon hexa cellid + storage path
+    df = utils.get_df_from_csv(root_folder, csv_name)
+    df = utils.add_storage_path(df)
 
-    preprocess(root_folder, csv_name)
-
-    # img = utils.get_image_from_npy(root_folder, 'e3_8b_5898198058.npy')
-    # utils.tag_image(img)
+    preprocess(root_folder, csv_name, df)
