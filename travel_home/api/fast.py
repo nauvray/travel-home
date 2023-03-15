30710166fd67fcfe92a2defd01e276dd0f02bb1a
maddalen lepphaille <maddalen.lepphaille@gmail.com>
2023-03-15 17:53:28 +0100

    modif for docker

--- travel_home/api/fast.py
+++ travel_home/api/fast.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from travel_home.ml_logic import registry
+from travel_home.ml_logic.registry import load_travel_home_model
 from travel_home.ml_logic import model as md
 from travel_home.params import *
 from PIL import Image
@@ -7,12 +7,14 @@ import validators
 import requests
 
 app = FastAPI()
+app.state.model = load_travel_home_model()
 
 # http://127.0.0.1:8000/predict?image=https://img.traveltriangle.com/blog/wp-content/uploads/2017/10/Cover10.jpg
+# http://127.0.0.1:8000/predict?image=../../00-data/seychelles.jpg
 @app.get("/predict")
 def predict(image):
-    # load latest model saved in gcs
-    model = registry.load_travel_home_model()
+    # get stored model
+    model = app.state.model
     assert model is not None
 
     if (validators.url(image)):
