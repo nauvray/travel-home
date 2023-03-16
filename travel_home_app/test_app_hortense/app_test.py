2bfc44976d0aa53d86abec2cd961454ff39cc71b
Hortense Jallot <120584238+hortense-jallot@users.noreply.github.com>
2023-03-16 15:45:10 +0100

    Update app_test.py

--- travel_home_app/test_app_hortense/app_test.py
+++ travel_home_app/test_app_hortense/app_test.py
@@ -65,7 +65,7 @@ if get_prediction==True:
     if image_uploaded is None :
         response = requests.get(list_link[clicked])
         image = Image.open(BytesIO(response.content))
-        image.save('image.jpg')
+        image.save('../../travel_home/api/image.jpg')
         image = 'image.jpg'
     else :
         image = image_uploaded
@@ -73,7 +73,7 @@ if get_prediction==True:
 # API
 if get_prediction == True:
     url = 'https://travel-home-mzfiw6j4fa-ew.a.run.app/predict'
-    params = {'image': f'../../travel_home_app/test_app_hortense/{image}'}
+    params = {'image': image}
     st.write(params)
     st.write(type(image))
     request = requests.get(url, params=params)
